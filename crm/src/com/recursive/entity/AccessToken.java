c651a733e0e0f2acd915c5bdf875c0abba110a95
Administrator <Administrator@RLR1FJ6AYMQZJFE>
2016-10-25 15:38:43 +0800

    完整

--- crm/src/com/recursive/entity/AccessToken.java
+++ crm/src/com/recursive/entity/AccessToken.java
@@ -2,24 +2,24 @@ package com.recursive.entity;
 
 public class AccessToken {
 	/**
-	 * 定义token
+	 * 
 	 */
 	private String access_token;
 	/**
-	 * token 超时时间
+	 * 
 	 */
-	private int expires_in;//�?
+	private int expires_in;//
 	/**
-	 * token创建时间
+	 * 
 	 * 
 	 */
 	private long createtime=System.currentTimeMillis();
 	/**
-	 * 判断是否超时
-	 * @return 返回true表示超时，false表示没有超时
+	 * 
+	 * 
 	 */
 	public boolean isExpires(){
-		return (System.currentTimeMillis()-createtime)/1000>=expires_in-60*15;//容错时间
+		return (System.currentTimeMillis()-createtime)/1000>=expires_in-60*15;//
 	}
 	
 	public String getAccess_token() {
--- crm/src/com/recursive/util/Constant.java
+++ crm/src/com/recursive/util/Constant.java
@@ -7,9 +7,6 @@ public class Constant {
 	public static final String MSG_TYPE_EVENT="event";
 	public static final String MSG_EVENT_CLICK="CLICK";
 	public static final String MSG_EVENT_SUBSCRIBE="subscribe";
-	/**
-	 * 带参数的二维码订�?
-	 */
 	public static final String MSG_EVENT_SUB_QRSCENE="qrscene_";
 	public static final String CREATE_PIC="createpic";
 }
